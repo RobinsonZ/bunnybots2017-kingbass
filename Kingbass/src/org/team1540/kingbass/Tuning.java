f7829092f24199269aed19c9a76c4c33803c2690
RobinsonZ <robinsonz@catlin.edu>
2017-10-04 15:15:12 -0700

    remove final flag from all tuning fields

--- Kingbass/src/org/team1540/kingbass/Tuning.java
+++ Kingbass/src/org/team1540/kingbass/Tuning.java
@@ -8,77 +8,77 @@ package org.team1540.kingbass;
 public class Tuning {
 
   // drivetrain
-  private static final boolean L_REVERSE_OUTPUT = false;
-  private static final boolean R_REVERSE_OUTPUT = false;
-  private static final boolean L_REVERSE_SENSOR = false;
-  private static final boolean R_REVERSE_SENSOR = false;
+  private static boolean lReverseOutput = false;
+  private static boolean rReverseOutput = false;
+  private static boolean lReverseSensor = false;
+  private static boolean rReverseSensor = false;
   // Arm
   /** Motor set point (out of 1) for moving the arm up and down. */
-  private static final double ARM_SPEED = 1;
+  private static double armSpeed = 1;
 
   // Autoshifting
   /** Current threshold below which the autoshifter will shift up. */
-  private static final double AUTO_SHIFT_DEVIATION_THRESHOLD = 3;
+  private static double autoShiftDeviationThreshold = 3;
 
   /**
    * The time, in ticks (about 20ms), between the autoshifter changing gearss.
    */
-  private static final int AUTOSHIFT_COOLDOWN = 5;
+  private static int autoshiftCooldown = 5;
 
   // claw
-  private static final double OPEN_CLAW_TIME = 1;
-  private static final double CLOSE_CLAW_TIME = 1;
+  private static double openClawTime = 1;
+  private static double closeClawTime = 1;
 
 
   // Intake
   /** Motor set point (out of 1) for running the intake in and out. */
-  private static final double INTAKE_SET_POINT = 1;
+  private static double intakeSetPoint = 1;
 
   // Input
   /** Axis input that is less than this value in either direction will be ignored. */
-  private static final double DEADZONE = 0.1;
+  private static double deadzone = 0.1;
 
   public static boolean lReverseOutput() {
-    return L_REVERSE_OUTPUT;
+    return lReverseOutput;
   }
 
   public static boolean rReverseOutput() {
-    return R_REVERSE_OUTPUT;
+    return rReverseOutput;
   }
 
   public static boolean lReverseSensor() {
-    return L_REVERSE_SENSOR;
+    return lReverseSensor;
   }
 
   public static boolean rReverseSensor() {
-    return R_REVERSE_SENSOR;
+    return rReverseSensor;
   }
 
   public static double getArmSpeed() {
-    return ARM_SPEED;
+    return armSpeed;
   }
 
   public static double getAutoShiftDeviationThreshold() {
-    return AUTO_SHIFT_DEVIATION_THRESHOLD;
+    return autoShiftDeviationThreshold;
   }
 
   public static int getAutoshiftCooldown() {
-    return AUTOSHIFT_COOLDOWN;
+    return autoshiftCooldown;
   }
 
   public static double getOpenClawTime() {
-    return OPEN_CLAW_TIME;
+    return openClawTime;
   }
 
   public static double getCloseClawTime() {
-    return CLOSE_CLAW_TIME;
+    return closeClawTime;
   }
 
   public static double getIntakeSetPoint() {
-    return INTAKE_SET_POINT;
+    return intakeSetPoint;
   }
 
   public static double getDeadzone() {
-    return DEADZONE;
+    return deadzone;
   }
 }
