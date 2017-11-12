e00d77395f17f0d5c99e41cf3bbfd2e99b431923
RobinsonZ <robinsonz@catlin.edu>
2017-11-12 08:39:02 -0800

    add gradle-wrapper.jar and revised lineUpIndices()

--- src/org/team1540/lib/Utilities.java
+++ src/org/team1540/lib/Utilities.java
@@ -12,10 +12,11 @@ public class Utilities {
   /**
    * Calculates the expected motor velocity.
    *
-   * @deprecated Probably doesn't work
    * @param gear The gear the robot is currently in ({@code false} for low, {@code true} for high)
    *
    * @return The expected motor velocity, in revolutions per minute.
+   *
+   * @deprecated Probably doesn't work
    */
   public static double calcExpectedRPM(boolean gear, double throttle) {
     return RobotInfo.CIM_FREE_SPEED * throttle
@@ -26,10 +27,11 @@ public class Utilities {
    * Calculates the difference (from 0 to 1) between the expected velocity based on the motor
    * throttle and the actual velocity from the encoder.
    *
-   * @deprecated Probably doesn't work
    * @param gear The gear the robot is currently in ({@code false} for low, {@code true} for high)
    * @param throttle The current power level of the motor.
    * @param encoderTalon The {@code CANTalon} that the encoder is connected to.
+   *
+   * @deprecated Probably doesn't work
    */
   public static double calcRPMDeviation(boolean gear, double throttle, CANTalon encoderTalon) {
     encoderTalon.configEncoderCodesPerRev(RobotInfo.ENCODER_CODES_PER_REV);
@@ -41,8 +43,10 @@ public class Utilities {
 
   /**
    * Processes an axis and returns the value only if it is outside the provided deadzone.
+   *
    * @param axis The axis to return
    * @param deadzone The deadzone to use.
+   *
    * @return If |{@code axis}| > |{@code deadzone}|, returns {@code axis}; otherwise, returns 0.
    */
   public static double processAxisDeadzone(double axis, double deadzone) {
@@ -51,19 +55,22 @@ public class Utilities {
 
   public static int[] lineUpIndices(int aLength, int bLength) {
     if (aLength == bLength) { return new int[]{0, 0, aLength, bLength}; }
-    int[] out = new int[4];
+    int[] out;
     if (aLength > bLength) {
-      out[0] = (aLength / 2) - (bLength / 2);
-      out[1] = 0;
-      out[2] = (aLength / 2) + (bLength / 2) + 1;
-      out[3] = bLength;
+      out = new int[]{
+          (aLength / 2) - (bLength / 2),
+          0,
+          (aLength / 2) + (bLength / 2) + 1,
+          bLength
+      };
     } else {
-      out[0] = 0;
-      out[1] = (bLength / 2) - (aLength / 2);
-      out[2] = aLength;
-      out[3] = (bLength / 2) + (aLength / 2) + 1;
+      out = new int[]{
+          0,
+          (bLength / 2) - (aLength / 2),
+          aLength,
+          (bLength / 2) + (aLength / 2) + 1
+      };
     }
     return out;
   }
-
 }
