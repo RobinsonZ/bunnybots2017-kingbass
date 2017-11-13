d79a132cd95e945a28ca4d834c8625c6ddea89c3
RobinsonZ <robinsonz@catlin.edu>
2017-11-13 15:00:54 -0800

    fixed DPadButton up not working
    it's ugly now, but that's ok

--- src/org/team1540/lib/triggers/DPadButton.java
+++ src/org/team1540/lib/triggers/DPadButton.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 
 /**
- * Used to map a button from the D-Pad on a controller (or any joystick POV device) to a button so
+ * Used to map a button from the D-Pad on a controller to a button so
  * it can be used for triggering commands.
  */
 public class DPadButton extends Button {
@@ -12,15 +12,7 @@ public class DPadButton extends Button {
    * Enum representing the possible axes of a D-Pad.
    */
   public enum DPadAxis {
-    UP(315, 45), DOWN(135, 225), LEFT(225, 315), RIGHT(45, 135);
-
-    final int min;
-    final int max;
-
-    DPadAxis(int min, int max) {
-      this.min = min;
-      this.max = max;
-    }
+    UP, DOWN, LEFT, RIGHT
   }
 
   private Joystick stick;
@@ -53,6 +45,17 @@ public class DPadButton extends Button {
    * Helper function so that it can be properly unit-tested. Should not be called directly.
    */
   public static boolean process(int pov, DPadAxis axis) {
-    return pov != -1 && (pov >= axis.min && pov <= axis.max);
+    switch (axis) {
+      case UP:
+        return pov == 315 || pov == 0 || pov == 45;
+      case DOWN:
+        return pov == 135 || pov == 180 || pov == 225;
+      case LEFT:
+        return pov == 225 || pov == 270 || pov == 315;
+      case RIGHT:
+        return pov == 45 || pov == 90 || pov == 135;
+      default:
+        return false;
+    }
   }
 }
