e99f689117b1d1fc44c1c8de53b9acb29a4ec822
RobinsonZ <robinsonz@catlin.edu>
2017-11-14 14:25:57 -0800

    implement more telemetry types
    AdjustableManager can now can display data from subclasses/implementors
    of WPILib gyros, counters/encoders, solenoids, accelerometers,
    potentiometers, and speed controllers. Still needs to be able to display
    data from NavXs and CANTalons as well as tune CANTalon PIDs.

--- src/org/team1540/lib/adjustables/TelemetryType.java
+++ src/org/team1540/lib/adjustables/TelemetryType.java
@@ -1,16 +1,23 @@
 package org.team1540.lib.adjustables;
 
+import edu.wpi.first.wpilibj.CounterBase;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
+
 public enum TelemetryType {
-  STRING(String.class), INT(Integer.TYPE), DOUBLE(Double.TYPE), BOOLEAN(Boolean.TYPE);
-  /*
-  TODO: Implement more possible types
+  STRING(String.class), INT(Integer.TYPE), DOUBLE(Double.TYPE), BOOLEAN(Boolean.TYPE),
+  //TODO: Implement more possible types
   SOLENOID(Solenoid.class),
-  DOUBLE_SOLENOID(DoubleSolenoid.class),
+  // DOUBLE_SOLENOID(DoubleSolenoid.class),
   SPEED_CONTROLLER(SpeedController.class),
   ACCELEROMETER(Accelerometer.class),
   GYRO(Gyro.class),
   POTENTIOMETER(Potentiometer.class),
-  COUNTER(CounterBase.class),
+  COUNTER(CounterBase.class);
+  /*
   TALON(CANTalon.class),
   NAVX(AHRS.class);
   */
