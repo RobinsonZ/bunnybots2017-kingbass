b39aa4cb7bab162144f88c6b757693c9d77bfdf7
RobinsonZ <robinsonz@catlin.edu>
2017-11-17 22:25:34 -0800

    add static imports
    I love static imports

--- src/org/team1540/kingbass/commands/intake/IntakeBunny.java
+++ src/org/team1540/kingbass/commands/intake/IntakeBunny.java
@@ -1,19 +1,20 @@
 package org.team1540.kingbass.commands.intake;
 
 import static org.team1540.kingbass.Robot.intake;
+import static org.team1540.kingbass.Tuning.intakeStopThresh;
+import static org.team1540.kingbass.Tuning.intakeTimeout;
 
 import edu.wpi.first.wpilibj.command.Command;
-import org.team1540.kingbass.Tuning;
 
 public class IntakeBunny extends Command {
 
   public IntakeBunny() {
-    super("Intake Bunny", Tuning.intakeTimeout);
+    super("Intake Bunny", intakeTimeout);
   }
 
   @Override
   protected boolean isFinished() {
-    return intake.getCurrent() >= Tuning.intakeStopThresh;
+    return intake.getCurrent() >= intakeStopThresh;
   }
 
   @Override
