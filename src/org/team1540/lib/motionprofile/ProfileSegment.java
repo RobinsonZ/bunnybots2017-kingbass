60e137fcc1566562ad31e6293ec121ce45b78307
RobinsonZ <robinsonz@catlin.edu>
2017-11-19 15:51:44 -0800

    add documentation

--- src/org/team1540/lib/motionprofile/ProfileSegment.java
+++ src/org/team1540/lib/motionprofile/ProfileSegment.java
@@ -1,9 +1,30 @@
 package org.team1540.lib.motionprofile;
 
+/**
+ * A segment of a motion profile.
+ */
 public interface ProfileSegment {
+
+  /**
+   * Gets the left-side output of this part of the motion profile, as an array of {@code double[]}
+   * formatted as {@code [position, velocity, duration]}.
+   *
+   * @return The left-side output of this motion profile.
+   */
   public double[][] getLeftPts();
 
+  /**
+   * Gets the right-side output of this part of the motion profile, as an array of {@code double[]}
+   * formatted as {@code [position, velocity, duration]}.
+   *
+   * @return The right-side output of this motion profile.
+   */
   public double[][] getRightPts();
 
+  /**
+   * Gets the number of points in the motion profile.
+   *
+   * @return The size of the profile.
+   */
   public int getSize();
 }
--- src/org/team1540/lib/motionprofile/ProfileSegmentChain.java
+++ src/org/team1540/lib/motionprofile/ProfileSegmentChain.java
@@ -2,20 +2,56 @@ package org.team1540.lib.motionprofile;
 
 import java.util.List;
 
+/**
+ * A chain of {@link org.team1540.lib.motionprofile.ProfileSegment} instances. Multiple
+ * ProfileSegments (either directly instantiated or produced by a {@link ProfileSegmentFactory} can
+ * be added to the chain before combining them into two arrays for the left and right motors using
+ * {@link #compileLeft()} and {@link #compileRight()}. This allows for complex profiles to be made
+ * using a combination of different kinds of {@code ProfileSegment}s.
+ *
+ * @see ProfileSegment
+ * @see ProfileSegmentFactory
+ */
 public class ProfileSegmentChain {
   private List<ProfileSegment> segments;
   // cache compilation result
   private double[][] leftCache;
   private double[][] rightCache;
 
-  public void add(ProfileSegment s) {
+  /**
+   * Adds a {@link org.team1540.lib.motionprofile.ProfileSegment} to the chain. The segment is added
+   * to the end of the chain and will be executed after any segments that have been already added.
+   *
+   * <p> This method returns a reference to its instance in a builder pattern. This allows multiple
+   * method calls to be chained together: <pre>
+   *   ProfileSegmentChain chain = new ProfileSegmentChain()
+   *    .add(segment1)
+   *    .add(segment2)
+   *    .add(segment3);
+   * </pre>
+   *
+   * @param s The segment to add.
+   */
+  public ProfileSegmentChain add(ProfileSegment s) {
     segments.add(s);
 
     // caches are invalid now, reset them
     leftCache = null;
     rightCache = null;
+
+    return this;
   }
 
+  /**
+   * Compiles the left-side output of the motion profile into a single array of points in the format
+   * {@code [position, velocity, duration]}. This method caches its output, and as such if it is
+   * called multiple times without the makeup of the chain changing through {@link
+   * #add(ProfileSegment)}, it will not re-compile the motion profile and instead return a reference
+   * to the same array as the last time it was called.
+   *
+   * @return An array of {@code double[]} instances each representing one point in the left-side
+   * motion profile, and formatted as {@code [position, velocity, duration]}.
+   */
   public double[][] compileLeft() {
     if (leftCache != null) { return leftCache; }
 
@@ -38,6 +74,16 @@ public class ProfileSegmentChain {
     return leftCache;
   }
 
+  /**
+   * Compiles the right-side output of the motion profile into a single array of points in the
+   * format {@code [position, velocity, duration]}. This method caches its output, and as such if it
+   * is called multiple times without the makeup of the chain changing through {@link
+   * #add(ProfileSegment)}, it will not re-compile the motion profile and instead return a reference
+   * to the same array as the last time it was called.
+   *
+   * @return An array of {@code double[]} instances each representing one point in the right-side
+   * motion profile, and formatted as {@code [position, velocity, duration]}.
+   */
   public double[][] compileRight() {
     if (rightCache != null) { return rightCache; }
 
