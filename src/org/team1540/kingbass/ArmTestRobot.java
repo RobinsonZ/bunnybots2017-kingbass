f593ace0da9bbb488ad437635d1e2942ced9041d
RobinsonZ <robinsonz@catlin.edu>
2017-11-30 18:46:55 -0800

    fix and improve ArmTestRobot
    Now can set a predefined position for the arm to PID to, and outputs
    additional telemetry (current output and whether it's in current
    limiting mode)

--- src/org/team1540/kingbass/ArmTestRobot.java
+++ src/org/team1540/kingbass/ArmTestRobot.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.team1540.base.adjustables.AdjustableManager;
 import org.team1540.kingbass.subsystems.Arm;
 
 public class ArmTestRobot extends IterativeRobot {
@@ -13,14 +14,29 @@ public class ArmTestRobot extends IterativeRobot {
 
   @Override
   public void robotInit() {
+    AdjustableManager.getInstance().add(new Tuning());
     arm = new Arm();
     button = new JoystickButton(new Joystick(0), 1);
+    SmartDashboard.putNumber("Position setpoint", arm.getPositionA());
   }
 
   @Override
   public void robotPeriodic() {
+    AdjustableManager.getInstance().update();
     SmartDashboard.putNumber("A position", arm.getPositionA());
-    SmartDashboard.putNumber("B position", arm.getPositionB());
-    if (button.get()) { arm.zeroPosition(); }
+    SmartDashboard.putNumber("A current", arm.getCurrentA());
+    SmartDashboard.putNumber("B current", arm.getCurrentB());
+    SmartDashboard.putBoolean("Current limited", arm.isCurrentLimited());
+    if (button.get()) {
+      arm.zeroPosition();
+    }
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    arm.setPosition(SmartDashboard.getNumber("Position setpoint", arm.getPositionA()));
+    if (button.get()) {
+      arm.setPosition(0);
+    }
   }
 }
