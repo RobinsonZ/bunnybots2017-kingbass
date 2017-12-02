ab5ad9d7cb94edfbef6f7d66eb291a80aed2d0db
RobinsonZ <robinsonz@catlin.edu>
2017-12-02 14:38:43 -0800

    make drive go the right way

--- src/org/team1540/kingbass/commands/drivetrain/JoystickDrive.java
+++ src/org/team1540/kingbass/commands/drivetrain/JoystickDrive.java
@@ -21,9 +21,10 @@ public class JoystickDrive extends Command {
 
   @Override
   protected void execute() {
-    driveTrain.setLeftMotors(processAxisDeadzone(getDriveLeftJoystick(),
+    // Don't ask
+    driveTrain.setRightMotors(-processAxisDeadzone(getDriveRightJoystick(),
         deadzone));
-    driveTrain.setRightMotors(processAxisDeadzone(getDriveRightJoystick(),
+    driveTrain.setLeftMotors(-processAxisDeadzone(getDriveLeftJoystick(),
         deadzone));
   }
 
