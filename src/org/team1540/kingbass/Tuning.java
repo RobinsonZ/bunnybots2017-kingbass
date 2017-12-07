881447ae25b1644e83557bcf619fc3808b969092
RobinsonZ <robinsonz@catlin.edu>
2017-12-07 10:59:41 -0800

    clean up Tuning
    some stuff is unnecessary, some stuff has already been tuned so doesn't
    need to be on the dashboard

--- src/org/team1540/kingbass/Tuning.java
+++ src/org/team1540/kingbass/Tuning.java
@@ -10,23 +10,23 @@ import org.team1540.base.adjustables.Tunable;
  */
 public class Tuning {
 
-  @Tunable("Vision P")
+  //@Tunable("Vision P")
   public static double visionP = 0.5;
-  @Tunable("Return Threshold")
+  //@Tunable("Return Threshold")
   public static double returnThreshold = 500;
   // advanced drive
-  @Tunable("Velocity History Size")
+  //@Tunable("Velocity History Size")
   public static int velHistorySize = 5;
 
   // motion profiling
   //values should be changed, these are just copied from henny's code
-  @Tunable("Profile P")
+  //@Tunable("Profile P")
   public static double profileP = 0.1;
-  @Tunable("Profile I")
+  //@Tunable("Profile I")
   public static double profileI = 0.00001;
-  @Tunable("Profile D")
+  //@Tunable("Profile D")
   public static double profileD = 0.1;
-  @Tunable("Profile F")
+  //@Tunable("Profile F")
   public static double profileF = 7.795;
 
   @Tunable("Claw P")
@@ -36,27 +36,16 @@ public class Tuning {
   @Tunable("Claw D")
   public static double clawD = 0.1;
   // drivetrain
-  @Tunable("Left reverse output")
+  //@Tunable("Left reverse output")
   public static boolean lReverseOutput = false;
-  @Tunable("Right reverse output")
+  //@Tunable("Right reverse output")
   public static boolean rReverseOutput = false;
-  @Tunable("Left reverse sensor")
+  //@Tunable("Left reverse sensor")
   public static boolean lReverseSensor = false;
   //@Tunable("Right reverse sensor")
   public static boolean rReverseSensor = false;
   //@Tunable("Intake timeout")
   public static double intakeTimeout = 5;
-  // Arm
-  /**
-   * Motor set point (out of 1) for moving the arm up and down.
-   */
-  @Tunable("Arm set point")
-  public static double armSpeed = 1;
-
-  // claw
-  public static double openClawTime = 1;
-  public static double closeClawTime = 1;
-
 
   // Intake
   /**
@@ -65,14 +54,14 @@ public class Tuning {
   //@Tunable("Intake setpoint")
   public static double intakeSetPoint = 0.5;
 
-  @Tunable("Intake stop threshold")
+  //@Tunable("Intake stop threshold")
   public static double intakeStopThresh = 7.5;
 
   // Input
   /**
    * Axis input that is less than this value in either direction will be ignored.
    */
-  @Tunable("Deadzone")
+  //@Tunable("Deadzone")
   public static double deadzone = 0.1;
   //@Tunable("Intake min time")
   public static double intakeMinTime = 1;
