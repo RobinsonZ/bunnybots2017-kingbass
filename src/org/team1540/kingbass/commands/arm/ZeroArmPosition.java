016ac4ae8db0171f708aafc5333192b20622e11f
RobinsonZ <robinsonz@catlin.edu>
2017-12-07 18:52:40 -0800

    make ZeroArmPosition safer

--- src/org/team1540/kingbass/commands/arm/ZeroArmPosition.java
+++ src/org/team1540/kingbass/commands/arm/ZeroArmPosition.java
@@ -7,6 +7,10 @@ import edu.wpi.first.wpilibj.command.InstantCommand;
 public class ZeroArmPosition extends InstantCommand {
 
   public ZeroArmPosition() {
+    // this makes it stop the arm control for a cycle.
+    // if this wasn't here and you zeroed it in teleop the arm would immediately yeet to
+    // a position a while away from where it actually should be
+    requires(arm);
     setRunWhenDisabled(true);
   }
 
